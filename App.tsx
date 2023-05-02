e32823fc323e224fb1bab7e75ab1134ce7d5f4ed
F3rn4nd0o <99568451+F3rn4nd0o@users.noreply.github.com>
2023-05-02 11:45:02 -0300

    Segundo Commit

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import { StatusBar } from 'react-native';
-import OneSignal from "react-native-onesignal";
+import { useEffect } from 'react';
+import { StatusBar, Platform } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -7,13 +8,38 @@ import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
+import { tagUserInfoCreate } from './src/notifications/notificationsTags';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
-OneSignal.setAppId('bf9c02c5-e142-44ac-841b-0849e0b675a3')
+
+const oneSignalAppId = Platform.OS === 'ios' ? '2b542c58-e2e9-48f1-8bff-004caefe40c4' : 'dd6753a9-19ad-4f0f-b1c3-700cff7f9dcc';
+OneSignal.setAppId(oneSignalAppId);
+
+OneSignal.promptForPushNotificationsWithUserResponse()
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  
+  tagUserInfoCreate();
+
+  useEffect(() => {
+    const unsubscribe = OneSignal.setNotificationOpenedHandler((response) => {
+
+      const { actionId } = response.action as any;
+
+      switch(actionId) {
+        case '1':
+          return console.log('Ver todas');
+        case '2':
+          return console.log('Ver pedido');
+        default:
+          return console.log('Não foi clicado em botão de ação');
+      }
+    })
+
+    return () => unsubscribe
+  },[]);
 
   return (
     <NativeBaseProvider theme={THEME}>
@@ -25,6 +51,8 @@ export default function App() {
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
+
+      
     </NativeBaseProvider>
   );
 }
