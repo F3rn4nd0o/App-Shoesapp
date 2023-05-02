6a5174b1adea5e2de190022b4441084028e99f3c
F3rn4nd0o <99568451+F3rn4nd0o@users.noreply.github.com>
2023-05-02 11:50:14 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -12,11 +12,7 @@ import { tagUserInfoCreate } from './src/notifications/notificationsTags';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
-
-const oneSignalAppId = Platform.OS === 'ios' ? '2b542c58-e2e9-48f1-8bff-004caefe40c4' : 'dd6753a9-19ad-4f0f-b1c3-700cff7f9dcc';
-OneSignal.setAppId(oneSignalAppId);
-
-OneSignal.promptForPushNotificationsWithUserResponse()
+OneSignal.setAppId("bf9c02c5-e142-44ac-841b-0849e0b675a3");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
